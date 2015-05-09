03c962dbbab2e2f6e4127cb813751a48dc645c8d
Breena Theseira <breenatheseira@yahoo.com>
2015-05-09 17:53:52 +0800

    Altered: R code to ensure it is working from console

--- R/damcat.R
+++ R/damcat.R
@@ -1,15 +1,29 @@
 # This app can change values daily:
 # Day 1, Month: Jan, River: Galas, Dabong, Wind: False, Rain: No - Very Heavy & Extreme
 
-damcat <- function(daySelect = "1", monthSelect = "January", riverSelect = "Sungai Galas, Dabong", rainSelect = "1", windSelect = "True") {
-  dam_prediction <- read.table("./River.csv",header=TRUE,sep=",") 
-  dam_prediction$Wind<- recode(dam_prediction$Wind, "0:1.5='False'; 1.6:21.0='True'")
-  
-  fit <- rpart(Rank_Dam ~ Day + Month + River + Wind + Rainfall ,method="class", data=dam_prediction,control=rpart.control(minsplit=4))
-  
-  plotdiv <- renderPlot({
-    rpart.plot(fit, type=4, extra=1)
-  })
-  print(plotdiv)
-  invisible()
+setwd(".")
+
+library(car)
+library("rpart")
+library("rpart.plot")
+library("ggplot2")
+
+dam_prediction <- read.table("./River.csv",header=TRUE,sep=",")
+
+dam_prediction$Wind<- recode(dam_prediction$Wind, "0:1.5='False'; 1.6:21.0='True'")
+
+fit <- rpart(Rank_Dam ~ Day + Month + River + Wind + Rainfall,method="class", data=dam_prediction,control=rpart.control(minsplit=4))
+
+damplot <- rpart.plot(fit, type=4, extra=1)
+
+damcategory <- function(daySelect = "1", monthSelect = "January", riverSelect = "Sungai Galas, Dabong", rainSelect = "1", windSelect = "True"){
+
+  newdata <- data.frame(
+    Day = as.numeric(daySelect),
+    Month = monthSelect,
+    River= riverSelect,
+    Rainfall =as.numeric(rainSelect),
+    Wind = windSelect)
+
+  return(result = predict(fit,newdata=newdata,type=c("class")))
 }
