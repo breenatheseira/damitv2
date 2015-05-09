e50256000648d31019ba8f5f8603943db6364b7f
Breena Theseira <breenatheseira@yahoo.com>
2015-05-09 19:33:41 +0800

    Updated: Converted csv file to rda file and altered R code to suit the reading from rda file

--- R/damcat.R
+++ R/damcat.R
@@ -8,11 +8,10 @@ library("rpart")
 library("rpart.plot")
 library("ggplot2")
 
-dam_prediction <- read.table("./River.csv",header=TRUE,sep=",")
+dam_prediction <- readRDS("./River.rda")
+#dam_prediction$Wind<- recode(dam_prediction$Wind, "0:1.5='False'; 1.6:21.0='True'")
 
-dam_prediction$Wind<- recode(dam_prediction$Wind, "0:1.5='False'; 1.6:21.0='True'")
-
-fit <- rpart(Rank_Dam ~ Day + Month + River + Wind + Rainfall,method="class", data=dam_prediction,control=rpart.control(minsplit=4))
+fit <- rpart(Rank_Dam ~ Day + Month + River + Wind + Rain,method="class", data=dam_prediction,control=rpart.control(minsplit=4))
 
 damplot <- rpart.plot(fit, type=4, extra=1)
 
