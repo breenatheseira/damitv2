914f80f3dfd7aa956cddc65eafa7dba10182c00b
Breena Theseira <breenatheseira@yahoo.com>
2015-09-08 02:18:41 +0800

    River results are being displayed

--- R/damcat.R
+++ R/damcat.R
@@ -8,9 +8,10 @@ library("ggplot2")
 
 dam_prediction <- get(load('./data/damitv2.rda'))
 
-dam_prediction$wind <- recode(dam_prediction$wind, "0:1.5='False'; 1.6:21.0='True'")
+dam_prediction$Wind<- recode(dam_prediction$Wind, "0:1.5='False'; 1.6:21.0='True'")
+dam_prediction$Rank_Dam <- recode(dam_prediction$Rank_Dam, "0:20='Dry'; 21:60='Critical Low'; 61:80='Average'; 81:99='Critical High';else='Overflow'")
 
-fit <- rpart(dam ~ month + station + wind + rain + year,method="class", data=dam_prediction,control=rpart.control(minsplit=4))
+fit <- rpart(Rank_Dam ~ Month + River + Wind + Rain,method="class", data=dam_prediction,control=rpart.control(minsplit=4))
 
 damplot <- function() {
   rpart.plot(fit, type=4, extra=1)
@@ -18,9 +19,8 @@ damplot <- function() {
 
 damcategory <- function(monthSelect = "1", riverSelect = "Butterworth", rainSelect = "1", windSelect = "True"){
 
-  newdata <- data.frame(
-    Day = as.numeric(daySelect),
-    Month = monthSelect,
+  newdata <- data.frame(    
+    Month = as.numeric(monthSelect),
     River= riverSelect,
     Rain =as.numeric(rainSelect),
     Wind = windSelect)
