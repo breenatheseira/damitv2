57a866a3a8a75960421fa29996c00fa85e37d6a9
Breena Theseira <breenatheseira@yahoo.com>
2015-09-08 01:58:31 +0800

    Changed data file to damit's original file & removed Year, Day selects. Graph keeps getting plotted; need results

--- R/damcat.R
+++ R/damcat.R
@@ -1,4 +1,4 @@
-# This app can change values daily:
+# This app can change values monthly:
 # Day 1, Month: Jan, River: Galas, Dabong, Wind: False, Rain: No - Very Heavy & Extreme
 
 library(car)
@@ -6,17 +6,17 @@ library("rpart")
 library("rpart.plot")
 library("ggplot2")
 
-dam_prediction <- get(load('./data/River.rda'))
+dam_prediction <- get(load('./data/damitv2.rda'))
 
-#dam_prediction$Wind<- recode(dam_prediction$Wind, "0:1.5='False'; 1.6:21.0='True'")
+dam_prediction$wind <- recode(dam_prediction$wind, "0:1.5='False'; 1.6:21.0='True'")
 
-fit <- rpart(Rank_Dam ~ Day + Month + River + Wind + Rain,method="class", data=dam_prediction,control=rpart.control(minsplit=4))
+fit <- rpart(dam ~ month + station + wind + rain + year,method="class", data=dam_prediction,control=rpart.control(minsplit=4))
 
 damplot <- function() {
   rpart.plot(fit, type=4, extra=1)
 }
 
-damcategory <- function(daySelect = "1", monthSelect = "January", riverSelect = "Sungai Galas, Dabong", rainSelect = "1", windSelect = "True"){
+damcategory <- function(monthSelect = "1", riverSelect = "Butterworth", rainSelect = "1", windSelect = "True"){
 
   newdata <- data.frame(
     Day = as.numeric(daySelect),
