085318ec4babc406dcfacc22c8371ab6289bb548
Breena Theseira <breenatheseira@yahoo.com>
2015-09-08 02:41:14 +0800

    Fixed: rda file path

--- R/damcat.R
+++ R/damcat.R
@@ -6,7 +6,7 @@ library("rpart")
 library("rpart.plot")
 library("ggplot2")
 
-dam_prediction <- get(load('./data/damitv2.rda'))
+dam_prediction <- get(load('./data/River.rda'))
 
 dam_prediction$Wind<- recode(dam_prediction$Wind, "0:1.5='False'; 1.6:21.0='True'")
 dam_prediction$Rank_Dam <- recode(dam_prediction$Rank_Dam, "0:20='Dry'; 21:60='Critical Low'; 61:80='Average'; 81:99='Critical High';else='Overflow'")
